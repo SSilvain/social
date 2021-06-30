0421f195a3817ce7ef527fb6c1e56e3c0a017c45
Serhii <silver383@gmail.com>
2021-06-30 22:52:46 +0300

    shallow copy

--- src/components/Dialogs/dialog-test.js
+++ src/components/Dialogs/dialog-test.js
@@ -1,3 +1,13 @@
 export const funcTest = (text) => {
-    alert(text);
+    let objFirst = { value: 0 };
+
+    let objSecond = objFirst;
+    // objSecond.name = "vasya";
+    objSecond = { value: 0 };
+
+    if (objFirst === objSecond) {
+        alert(text + ", not changed");
+    } else {
+        alert(text + ", yes, changed");
+    }
 };
