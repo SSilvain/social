b8f6a0a54524769980032b0d17d4073965833088
Serhii <silver383@gmail.com>
2021-07-01 11:09:35 +0300

    Object.entries

--- src/components/Dialogs/dialog-test.js
+++ src/components/Dialogs/dialog-test.js
@@ -1,13 +1,8 @@
 export const funcTest = (text) => {
-    let objFirst = { value: 0 };
-
-    let objSecond = objFirst;
-    // objSecond.name = "vasya";
-    objSecond = { value: 0 };
-
-    if (objFirst === objSecond) {
-        alert(text + ", not changed");
-    } else {
-        alert(text + ", yes, changed");
-    }
+    let obj = {
+        first: "one",
+        second: "two",
+    };
+    let arr = Object.entries(obj);
+    console.log(arr[0][1]);
 };
